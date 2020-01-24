cbcbab99b6a6ae555ffc39b6933325ed7011e65e
nokl <naoki.tkb@gmail.com>
2020-01-24 17:56:33 +0900

    add: main js and deps

---
+++ index.js
@@ -0,0 +1,59 @@
+const defaults = {
+    /** @type {boolean} */
+    init: true,
+    /** @type {String} target selector */
+    targetSelector: '.is-scroll-show',
+    /** @type {String} add class name */
+    addClass: 'is-show',
+    /** @type {Number[]} */
+    threshold: [0.25],
+    /** @type {boolean} */
+    isOnce: false,
+};
+
+export default class ScrollShowElm {
+    /**
+     * @param {defaults} props
+     */
+    constructor(props) {
+        const options = Object.assign({}, defaults, props);
+        this.init = options.init;
+        this.targetSelector = options.targetSelector;
+        this.addClass = options.addClass;
+        this.threshold = options.threshold;
+        this.isOnce = options.isOnce;
+
+        if (this.init) this.initialize();
+    }
+    initialize() {
+        /** @type {HTMLElement[]} */
+        this.targets = document.querySelectorAll(this.targetSelector);
+        if (this.targets.length > 0) {
+            this.start();
+        }
+    }
+    start() {
+        /** @type {IntersectionObserverInit} */
+        const options = {
+            threshold: this.threshold,
+        };
+        /**
+         * @param {IntersectionObserverEntry[]} entries
+         * @param {IntersectionObserver} observer
+         */
+        const callback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio >= this.threshold) {
+                    entry.target.classList.add(this.addClass);
+                    if (this.isOnce) observer.unobserve(entry.target);
+                } else if (!entry.isIntersecting) {
+                    entry.target.classList.remove(this.addClass);
+                }
+            });
+        };
+        const observer = new IntersectionObserver(callback, options);
+        this.targets.forEach(elm => {
+            observer.observe(elm);
+        });
+    }
+}
